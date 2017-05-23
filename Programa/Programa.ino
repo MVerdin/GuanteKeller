91cf7d0d7841000374f17cf820a97354bbeb544f
Mario Verdin <jmvl1303@gmail.com>
2017-05-22 21:47:10 -0500

    Establecidas direcciones de memoria EEPROM para datos

--- Programa/Programa.ino
+++ Programa/Programa.ino
@@ -1,21 +1,58 @@
-const int pinDedoIndice1=0
-const int pinDedoIndice2=1
-const int pinDedoMedio1=2
-const int pinDedoMedio2=3
-const int pinDedoAnular1=4
-const int pinDedoAnular2=5
-const int pinDedoMenique1=6
-const int pinDedoMenique2=7
-const int pinDedoPulgar1=8
-const int pinDedoPulgar2=9
+//Pines analogicos de conexion de potenciometros
+const int pinDedoIndice1=0;
+const int pinDedoIndice2=1;
+const int pinDedoMedio1=2;
+const int pinDedoMedio2=3;
+const int pinDedoAnular1=4;
+const int pinDedoAnular2=5;
+const int pinDedoMenique1=6;
+const int pinDedoMenique2=7;
+const int pinDedoPulgar1=8;
+const int pinDedoPulgar2=9;
 
-
+//Direcciones datos
+const int a=1;
+const int b=2;
+const int c=3;
+const int ch=4;
+const int d=5;
+const int e=6;
+const int f=7;
+const int g=8;
+const int h=9;
+const int i=10;
+const int j=11;
+const int k=12;
+const int l=13;
+const int m=14;
+const int n=15;
+const int ñ=16;
+const int o=17;
+const int p=18;
+const int q=19;
+const int r=20;
+const int s=21;
+const int t=22;
+const int u=23;
+const int v=24;
+const int w=25;
+const int x=26;
+const int y=27;
+const int z=28;
+const int inicioMemoria=512;
+//Modo de calibracion
+const boolean calibracion=true;
 
 
 
 void setup() {
-  // put your setup code here, to run once:
-
+  if (calibracion){
+    
+  }
+  else{
+    
+  }
+  
 }
 
 void loop() {
