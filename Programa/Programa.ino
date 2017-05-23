124ee85052d361637909f6d2fde5dccf766557d5
Mario Verdin <jmvl1303@gmail.com>
2017-05-22 21:18:25 -0500

    Pines de conexion establecidos

--- Programa/Programa.ino
+++ Programa/Programa.ino
@@ -1,3 +1,18 @@
+const int pinDedoIndice1=0
+const int pinDedoIndice2=1
+const int pinDedoMedio1=2
+const int pinDedoMedio2=3
+const int pinDedoAnular1=4
+const int pinDedoAnular2=5
+const int pinDedoMenique1=6
+const int pinDedoMenique2=7
+const int pinDedoPulgar1=8
+const int pinDedoPulgar2=9
+
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
